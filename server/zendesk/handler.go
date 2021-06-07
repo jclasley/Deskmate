7db84d4411187407debc145b28510eff185cf8c4
Tyler Conlee <tylerlconlee@gmail.com>
2021-06-07 11:47:23 -0700

    return when error met

--- server/zendesk/handler.go
+++ server/zendesk/handler.go
@@ -2,7 +2,6 @@ package zendesk
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -10,7 +9,8 @@ func ConnectHandler(w http.ResponseWriter, r *http.Request) {
 	payload := make(map[string]interface{})
 	err := json.NewDecoder(r.Body).Decode(&payload)
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Errorw("Error decoding JSON for Zendesk connect", "error", err.Error())
+		return
 	}
 	SetConfig()
 	Connect(payload["url"].(string))
@@ -20,6 +20,7 @@ func StatusHandler(w http.ResponseWriter, r *http.Request) {
 	js, err := json.Marshal(active)
 	if err != nil {
 		log.Errorw("Error marshalling JSON for config", "error", err.Error())
+		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
