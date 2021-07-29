11fe0cc87833848df2c9addc54b47d5ba2b395aa
Jon Lasley <jonlasley@Jons-MacBook-Pro.local>
2021-07-29 11:11:18 -0600

    server start boilerplate out

--- serverStart.sh
+++ serverStart.sh
@@ -2,4 +2,8 @@
 /go/bin/zendesk&
 /go/bin/server
 
+# somehowWait 3
 # need to hit connect endpoints of each server wiht a `curl`
+
+# curl localhost:8080/handler/slack/connect
+# curl localhost:8080/handler/zendesk/connect
