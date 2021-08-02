731b8d410dc610d7839203b07ea5cf886dd1a500
Jon Lasley <jonlasley@Jons-MacBook-Pro.local>
2021-08-02 14:43:28 -0600

    Trying other urls

--- serverStart.sh
+++ serverStart.sh
@@ -5,7 +5,7 @@
 sleep 5
 
 curl localhost:8080/handler/zendesk/connect \
-    -d '{"url": "localhost:8090/"}'
+    -d '{"url": "circleci.zendesk.com/"}'
 
 # moved to last so that we hang
 sleep infinity
