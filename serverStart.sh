1dd9c9a5c0305f1f0a10c5b517fed4a8446f465c
Jon Lasley <jonlasley@Jons-MacBook-Pro.local>
2021-08-02 15:05:42 -0600

    added in logging of time before and after sleep to verify sleep is running

--- serverStart.sh
+++ serverStart.sh
@@ -2,7 +2,11 @@
 /go/bin/server&
 /go/bin/zendesk&
 
+now=$(date +"%T")
+echo "Before sleep: $now"
 sleep 5
+now=$(date +"%T")
+echo "After sleep: $now"
 
 curl -f -sS -L localhost:8080/handler/zendesk/connect \
     -d '{"url": "circleci.zendesk.com/"}'
