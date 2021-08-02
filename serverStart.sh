8b324aa18498eaa5c0aa258e620ad387290dc52a
Jon Lasley <jonlasley@Jons-MacBook-Pro.local>
2021-08-02 15:20:45 -0600

    Added control curl to see what's up

--- serverStart.sh
+++ serverStart.sh
@@ -8,8 +8,9 @@ sleep 5
 now=$(date +"%T")
 echo "After sleep: $now"
 
+curl -f -v -L localhost:8080/handler/ # test any curl at all
 curl -f -v -L localhost:8080/handler/zendesk/connect \
-    -d '{"url": "circleci.zendesk.com/"}'
+    -d '{"url": "http://localhost:8090/"}'
 
 # moved to last so that we hang
 sleep infinity
